c4ed49fc264b2b6f9755d6a5eb21c8eb809d736a
prashant <prashant.brahmbhatt@tothenew.com>
2020-02-10 12:26:19 +0530

    added transition to buttons

--- run.py
+++ run.py
@@ -13,8 +13,9 @@ import matplotlib.pyplot as plt
 import cv2 
 
 app = Flask(__name__)
-#app.config['IMAGE_UPLOADS'] = 'C:\\Users\\ACER\\Documents\\workspace\\repositories\\Online_Lesion_Segmentation_Platform\\static'
-app.config['IMAGE_UPLOADS'] = '/home/prashant/git_repos_hashbanger/Online_Lesion_Segmentation_Platform/static'
+
+static_path = '/home/prashant/git_repos_hashbanger/Online_Lesion_Segmentation_Platform/static'
+app.config['IMAGE_UPLOADS'] = static_path
 
 @app.route('/')
 @app.route('/home')
@@ -30,12 +31,11 @@ def segment():
     if request.method == "POST":
         if request.files:
             image = request.files["image"]
-            # filename = str(image.filename)
             filename = "input_image.bmp"
             image.save(os.path.join(app.config['IMAGE_UPLOADS'], filename))
             print("Saved!")
             convert(filename)
-            # os.remove(r'C:\Users\ACER\Documents\workspace\skin lesion\Final_Year_Project\static\*.bmp')
+
             return redirect(request.url)
     return render_template('segment.html')
 
@@ -55,8 +55,7 @@ def get_segment_crop(img,tol=0, mask=None):
     return img[np.ix_(mask.any(1), mask.any(0))]
 
 def convert(filename):
-    # filename = os.path.join(r"C:\Users\ACER\Documents\workspace\repositories\Online_Lesion_Segmentation_Platform\static",filename)
-    filename = os.path.join(r"/home/prashant/git_repos_hashbanger/Online_Lesion_Segmentation_Platform/static",filename)
+    filename = os.path.join(static_path,filename)
     
     
     inp_image = np.array(Image.open(filename))
@@ -69,33 +68,21 @@ def convert(filename):
 
     im_1 = Image.fromarray(np.uint8(cm.gist_earth(img_pred)*255))
     im_1 = im_1.convert("L")
-    # im_1.save(r"C:\Users\ACER\Documents\workspace\repositories\Online_Lesion_Segmentation_Platform\static\segmented.bmp")
-    im_1.save(r"/home/prashant/git_repos_hashbanger/Online_Lesion_Segmentation_Platform/static/segmented.bmp")
+    im_1.save(os.path.join(static_path,'segmented.bmp'))
 
     dim = (256, 192)
     im_2 = cv2.resize(img_crop, dim, interpolation = cv2.INTER_AREA)
     im_2 = Image.fromarray(im_2)
     im_2 = im_2.convert("RGB")
-    # im_2.save(r"C:\Users\ACER\Documents\workspace\repositories\Online_Lesion_Segmentation_Platform\static\cropped.bmp")
-    im_2.save(r"/home/prashant/git_repos_hashbanger/Online_Lesion_Segmentation_Platform/static/cropped.bmp")
-
-
-
+    im_2.save(os.path.join(static_path, 'cropped.bmp'))
 
 @app.route('/download', methods = ['GET','POST'])
 def download():
-    # return send_file(r"C:\Users\ACER\Documents\workspace\repositories\Online_Lesion_Segmentation_Platform\static\segmented.bmp")
-    return send_file(r"/home/prashant/git_repos_hashbanger/Online_Lesion_Segmentation_Platform/static/segmented.bmp")
-
-
+    return send_file(os.path.join(static_path, 'segmented.bmp'))
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
-
-# @app.route('/return_File')
-# def return_File():
-#     return send_file(r"C:\Users\ACER\Documents\workspace\skin lesion\Final_Year_Project\static\segmented.bmp")
 
 @app.after_request
 def add_header(r):
